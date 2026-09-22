0c75e5cd20e81abba44b99f0e4940b64c5a56481
mubixd69-ux <mubixd69@gmail.com>
2026-09-22 18:57:33 +0300

    Made it so the taco dissapear
    whenever it gets out of the screen or gets collected

--- scripts/taco.gd
+++ scripts/taco.gd
@@ -1,4 +1,13 @@
 extends Area2D
 
+signal missed
+
 func _process(delta):
 	position.y += 200 * delta
+	
+	if position.y > 700:
+		missed.emit()
+		queue_free()
+		
+func get_caught() -> void:
+	queue_free()
