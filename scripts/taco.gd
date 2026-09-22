88fba64c803d7ea0d41b9a8643829132b6fae759
mubixd69-ux <mubixd69@gmail.com>
2026-09-22 19:12:20 +0300

    Made it so the higher the combo the higher the fall speed

--- scripts/taco.gd
+++ scripts/taco.gd
@@ -2,8 +2,13 @@ extends Area2D
 
 signal missed
 
+var fall_speed = 200.0
+
+func set_combo(combo: int):
+	fall_speed = min(200.0 + (combo * 35.0), 700)
+
 func _process(delta):
-	position.y += 200 * delta
+	position.y += fall_speed * delta
 	
 	if position.y > 700:
 		missed.emit()
