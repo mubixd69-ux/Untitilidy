d799c1eea5f0e2a1b329136e8dc2985e93f76c4a
theEerSOYEAH <85411618+theEerSOYEAH@users.noreply.github.com>
2026-09-22 22:32:36 +0300

    ignore

--- hud.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-@onready var taco_label = $CanvasLayer/TacoLabel
-
-var taco_count: int = 0
-
-func update_tacos(amount: int) -> void:
-	taco_count += amount
-	taco_label.text = "Tacos: " + str(taco_count)
