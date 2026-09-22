d56f61583ab008d5c75344c5007760e3ab844713
mubixd69-ux <mubixd69@gmail.com>
2026-09-22 18:58:11 +0300

    Tuned the taco spawn

--- scripts/spawner.gd
+++ scripts/spawner.gd
@@ -2,22 +2,28 @@ extends Node2D
 
 const TACO = preload("uid://oyjm4g70q2t7")
 
-func spawn_tacos():
-			var taco = TACO.instantiate()
-			taco.position = Vector2(
-			randf_range(50, 1150),
-			randf_range(-500, 0))
-			add_child(taco)
+@export var bag: Area2D
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var timer =Timer.new() 
-	timer.wait_time = 1
-	timer.timeout.connect(spawn_tacos)
-	add_child(timer)
-	timer.start()
+	spawn_tacos()
+
+func spawn_tacos() -> void:
+	var taco = TACO.instantiate()
+	taco.position = Vector2(
+		randf_range(50, 1150),
+		randf_range(-500, 0)
+	)
 	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	taco.missed.connect(_on_taco_missed)
+	taco.tree_exited.connect(_on_taco_despawned)
+	add_child(taco)
+
+func _on_taco_missed() -> void:
+	if bag and bag.has_method("reset_combo"):
+		bag.reset_combo()
+		
+func _on_taco_despawned() -> void:
+	var random_delay = randf_range(0.1, 1.2)
+	await get_tree().create_timer(random_delay).timeout
+	spawn_tacos()
+	
