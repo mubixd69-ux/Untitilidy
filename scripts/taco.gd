821bf5957ae93b73453f4756ff469db36d197fad
theEerSOYEAH <85411618+theEerSOYEAH@users.noreply.github.com>
2026-09-24 15:52:33 +0300

    improvement on taco script

--- scripts/taco.gd
+++ scripts/taco.gd
@@ -3,7 +3,11 @@ extends Area2D
 signal missed
 signal caught
 
+@export var taco_value: int = 1
+@export var ignores_multiplier: bool = false
+
 var fall_speed = 200.0
+
 
 func set_combo(combo: int):
 	fall_speed = min(200.0 + (combo * 35.0), 700)
@@ -15,8 +19,11 @@ func _process(delta):
 		missed.emit()
 		queue_free()
 		
-func get_caught() -> void:
-	GameData.add_taco()
+func get_caught(multiplier: int = 1) -> void:
+	if ignores_multiplier:
+		GameData.add_taco(taco_value)
+	else:
+		GameData.add_taco(taco_value, multiplier)
+
 	queue_free()
 	caught.emit()
-	
