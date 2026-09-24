c0c43977dc8ea0ade5ec94eadccd55a0b268d9d6
theEerSOYEAH <85411618+theEerSOYEAH@users.noreply.github.com>
2026-09-24 15:53:22 +0300

    imrpovement for gettaco function

--- scripts/GameData.gd
+++ scripts/GameData.gd
@@ -2,5 +2,5 @@ extends Node
 
 var taco_count: int = 0
 
-func add_taco() -> void:
-	taco_count += 1
+func add_taco(amount: int = 1, multipliar: int = 1) -> void:
+	taco_count += amount * multipliar
